64849adcbfe9ec5e7282b291c5d437d039085355
Patrick Rivera <pxr13@my.fsu.edu>
2018-03-30 14:26:04 -0400

    Removed files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-require('dotenv').config();
-import serverSetup from './init/server.js';
 import { initBot, initActions } from './init/';
 
 const bot = initBot();
-// serverSetup(bot);
 initActions(bot);
