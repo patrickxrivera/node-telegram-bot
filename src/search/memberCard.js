85026b22ee395ba3aad434c5f1ae79e7b1b6f7e7
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-06 04:27:06 -0400

    Refactored pt. 1 of member card

--- src/search/memberCard.js
+++ src/search/memberCard.js
@@ -1,26 +1,9 @@
 import reduce from 'lodash/reduce';
+import { pipe, curry, filter, head } from 'ramda';
 
 import attributes from '../utils/attributes.js';
 import links from '../data/links.js';
 import { toTitleCase } from '../utils/helpers.js';
-
-const getMemberCardFrom = (responses, name) => {
-  const targetMember = getTargetMember(responses, name);
-  const card = concatAttributesFor(targetMember);
-  return card;
-};
-
-const getTargetMember = (responses, name) =>
-  responses.filter((response) => response.name.trim() === name)[0];
-
-const concatAttributesFor = (person) => {
-  const card = reduce(
-    person,
-    (result, val, key) => result + getAttribute(key, val),
-    ''
-  );
-  return card;
-};
 
 const getAttribute = (key, val) => {
   key = toTitleCase(key);
@@ -30,6 +13,27 @@ const getAttribute = (key, val) => {
   const formattedCard = format(data);
 
   return formattedCard;
+};
+
+const concatAttributes = (person) => {
+  const card = reduce(
+    person,
+    (result, val, key) => result + getAttribute(key, val),
+    ''
+  );
+  return card;
+};
+
+const getTargetMember = curry(
+  (name, response) => response.name.trim() === name
+);
+
+const getCard = (name, responses) =>
+  pipe(filter(getTargetMember(name)), head, concatAttributes)(responses);
+
+const getMemberCardFrom = (responses, name) => {
+  const card = getCard(name, responses);
+  return card;
 };
 
 export default getMemberCardFrom;
