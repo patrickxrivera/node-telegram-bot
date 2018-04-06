b719fc9ba7661deac84d3dd7619d6ae46a9293ea
Patrick Rivera <pxr13@my.fsu.edu>
2018-04-06 03:18:10 -0400

    Refactored sendStart to be more functional

--- src/messages/sendStart.js
+++ src/messages/sendStart.js
@@ -1,18 +1,20 @@
+import { pipe } from 'ramda';
+
 import formatInline from '../utils/inlineKeyboard.js';
 import roles from '../data/roles.js';
 import { getIdFrom, getNameFrom } from '../utils/helpers.js';
 import { getStartText } from '../utils/text.js';
 
+const formatMarkup = (roles) => ({ reply_markup: { inline_keyboard: roles } });
+
+const getOptns = pipe(formatInline, formatMarkup);
+
+const getWelcomeText = pipe(getNameFrom, getStartText);
+
 const sendStart = (msg, bot) => {
   const id = getIdFrom(msg);
-  const name = getNameFrom(msg);
-  const text = getStartText(name);
-  const formattedRoles = formatInline(roles);
-  const optns = {
-    reply_markup: {
-      inline_keyboard: formattedRoles
-    }
-  };
+  const text = getWelcomeText(msg);
+  const optns = getOptns(roles);
   bot.sendMessage(id, text, optns);
 };
 
